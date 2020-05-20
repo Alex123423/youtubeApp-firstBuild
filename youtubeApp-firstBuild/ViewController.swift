b325bf07e4396893257b6cb78ec0ad905dc18bbd
AlexD <alexeydavidenko@icloud.com>
2020-05-20 22:07:45 +0300

    initial xcode project creation

---
+++ youtubeApp-firstBuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtubeApp-firstBuild
+//
+//  Created by Alexey Davidenko on 20.05.2020.
+//  Copyright © 2020 Alexey Davidenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
